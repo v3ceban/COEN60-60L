efde93585dbf898a0d95f631d47c166c71054c50
Vladimir Ceban <v3ceban@proton.me>
2023-10-11 00:45:35 -0700

    props update

--- A2/src/parts/header.jsx
+++ A2/src/parts/header.jsx
@@ -1,44 +1,44 @@
 import React from "react";
 
-const PropTypes = "pagename";
-
 export default function Header({ setPage }) {
-	return (
-		<header>
-			<h1>Vlads Schedule Page</h1>
-			<img src="https://placekitten.com/150/150" alt="vlad" />
-			<nav>
-				<ul>
-					<li>
-						<button onClick={() => setPage("Summary")}>Summary</button>
-					</li>
-					<li>
-						<button onClick={() => setPage("Sunday")}>Sunday</button>
-					</li>
-					<li>
-						<button onClick={() => setPage("Monday")}>Monday</button>
-					</li>
-					<li>
-						<button onClick={() => setPage("Tuesday")}>Tuesday</button>
-					</li>
-					<li>
-						<button onClick={() => setPage("Wednesday")}>Wednesday</button>
-					</li>
-					<li>
-						<button onClick={() => setPage("Thursday")}>Thursday</button>
-					</li>
-					<li>
-						<button onClick={() => setPage("Friday")}>Friday</button>
-					</li>
-					<li>
-						<button onClick={() => setPage("Saturday")}>Saturday</button>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	);
+  return (
+    <header>
+      <h1>Vlads Schedule Page</h1>
+      <img src="https://placekitten.com/150/150" alt="vlad" />
+      <nav>
+        <ul>
+          <li>
+            <button onClick={() => setPage("Summary")}>Summary</button>
+          </li>
+          <li>
+            <button onClick={() => setPage("Sunday")}>Sunday</button>
+          </li>
+          <li>
+            <button onClick={() => setPage("Monday")}>Monday</button>
+          </li>
+          <li>
+            <button onClick={() => setPage("Tuesday")}>Tuesday</button>
+          </li>
+          <li>
+            <button onClick={() => setPage("Wednesday")}>Wednesday</button>
+          </li>
+          <li>
+            <button onClick={() => setPage("Thursday")}>Thursday</button>
+          </li>
+          <li>
+            <button onClick={() => setPage("Friday")}>Friday</button>
+          </li>
+          <li>
+            <button onClick={() => setPage("Saturday")}>Saturday</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
+const PropTypes = "pagename";
+
 Header.propTypes = {
-	setPage: PropTypes.toString.isRequired,
+  setPage: PropTypes.toString.isRequired,
 };
