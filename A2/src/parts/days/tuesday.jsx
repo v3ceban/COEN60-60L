3f9ce664489545634f80db90c8b81692aa86c32c
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2023-10-11 00:28:22 -0700

    css and jsx updates

--- A2/src/parts/days/tuesday.jsx
+++ A2/src/parts/days/tuesday.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
 const Tuesday = () => {
-  return (
-    <section>
-      <h2>Tuesday</h2>
-    </section>
-  );
+	return (
+		<section>
+			<h2>Tuesday</h2>
+			<svg width="279" height="162" viewBox="0 0 279 162" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<rect width="279" height="162" rx="4" fill="#1E1E2E" />
+				<rect x="10" y="10" width="230" height="10" rx="5" fill="#F38BA8" />
+				<rect x="10" y="32" width="152" height="10" rx="5" fill="#FAB387" />
+				<rect x="10" y="54" width="188" height="10" rx="5" fill="#F9E2AF" />
+				<rect x="10" y="76" width="93" height="10" rx="5" fill="#B4BEFE" />
+				<rect x="10" y="98" width="213" height="10" rx="5" fill="#EBA0AC" />
+				<rect x="10" y="120" width="152" height="10" rx="5" fill="#A6E3A1" />
+				<rect x="10" y="142" width="244" height="10" rx="5" fill="#89B4FA" />
+			</svg>
+		</section>
+	);
 };
 
 export default Tuesday;
