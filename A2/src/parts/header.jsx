24c5b84d2a50e54023ac3d283f0b9fc9d5d613e1
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2023-10-12 23:46:02 -0700

    import from public fix

--- A2/src/parts/header.jsx
+++ A2/src/parts/header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../public/logo.png";
+import logo from "/logo.png";
 
 export default function Header({ setPage }) {
 	const [activeButtons, setActiveButtons] = useState({
