2f4ece167a3529b1c5c96556809593fa46ccabf6
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2023-10-19 23:02:54 -0700

    lab 5 probably done

--- Lab5/public/main.js
+++ Lab5/public/main.js
@@ -1,15 +1,54 @@
 const main = () => {
-  const button = document.querySelector("button");
+  const button = document.querySelectorAll("button");
   const svg = document.querySelector("svg");
+  const svgWidth = svg.width.baseVal.value;
+  const svgHeight = svg.height.baseVal.value;
+  const width = svgWidth / 20;
+  const height = svgHeight / 20;
+  let flag = false;
 
-  button.addEventListener("click", () => {
-    let square = document.createElement(`rect`);
-    square.setAttribute("x", "0");
-    square.setAttribute("y", "0");
-    square.setAttribute("width", "50");
-    square.setAttribute("heigth", "50");
-    square.setAttribute("fill", "red");
+  const randNum = (x) => {
+    return Math.floor(Math.random() * x);
+  };
+
+  const randColor = () => {
+    let a = "0123456789abcdef";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += a[randNum(a.length)];
+    }
+    return color;
+  };
+
+  const createSquare = () => {
+    let square = document.createElementNS(`http://www.w3.org/2000/svg`, `rect`);
+    square.setAttribute("x", randNum(svgWidth - width));
+    square.setAttribute("y", randNum(svgHeight - height));
+    square.setAttribute("width", width);
+    square.setAttribute("height", height);
+    square.setAttribute("fill", randColor());
+    return square;
+  };
+
+  button[0].addEventListener("click", () => {
+    let square = createSquare();
     svg.appendChild(square);
+  });
+  button[1].addEventListener("click", () => {
+    for (let index = 0; index < 5; index++) {
+      let square = createSquare();
+      svg.appendChild(square);
+    }
+  });
+  button[2].addEventListener("click", () => {
+    if (flag == false) {
+      flag = true;
+      alert("Because I can.");
+    }
+    for (let index = 0; index < 100; index++) {
+      let square = createSquare();
+      svg.appendChild(square);
+    }
   });
 };
 
